4b7eae2df278c92bfb600c04edd3af549bf3a33b
Sadat Rafi <sadatrafi1376@gmail.com>
2023-02-07 04:07:23 +0600

    Add files via upload

---
+++ 11 SPI HAL/main.c
@@ -0,0 +1,11 @@
+#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+#include "RTE_Device.h"                 // Keil::Device:STM32Cube Framework:Classic
+
+int main(void)
+{
+	while(1)
+	{
+	
+	}
+}
